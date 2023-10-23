ace99a66c88e095e480d5ce10750869e33d49f4b
daniela <gaofulou05@gmail.com>
2023-10-23 16:31:05 +0200

    compilacio

--- practica.c
+++ practica.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
-#includ <stdio.h>
+#include <stdio.h>
 #include <math.h>
 #define N 512
-#define float V1,V2,V3,V4 [N]
-#define flaot Mat, MatDD [N][N]
+ 
+float V1,V2,V3,V4 [N];
+ 
+float Mat,MatDD[N][N];
 
 void InitData(){
 	int i,j;
 	srand(8824553);
-	for( i = 0; i < N; i++ )
+	for( i = 0; i < N; i++ ){
 		for( j = 0; j < N; j++ ){
 			Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
 			if ( (abs(i - j) <= 3) && (i != j))
@@ -17,6 +19,7 @@ void InitData(){
 				 MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
 			else MatDD[i][j] = 0.0;
 		}
+	}
 	for( i = 0; i < N; i++ ){
 		V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
 		V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
@@ -24,7 +27,10 @@ void InitData(){
 	}
 }
 
-
 void PrintVect( float vect[N], int from, int numel ) {
-	int printf
-
+	int i = numel;
+	for ( i; i!=0; i-- ){
+		printf( "%f", vect[from] );
+		from++;
+	}
+}
