a9ed8fc86f7d73c9ffd77b6284609bb244451a8a
daniela <gaofulou05@gmail.com>
2023-10-23 16:05:13 +0200

    versio1

--- practica.sh
+++ practica.sh
@@ -1,3 +1,30 @@
 #include <stdlib.h>
 #includ <stdio.h>
 #include <math.h>
+#define N 512
+#define float V1,V2,V3,V4 [N]
+#define flaot Mat, MatDD [N][N]
+
+void InitData(){
+	int i,j;
+	srand(8824553);
+	for( i = 0; i < N; i++ )
+		for( j = 0; j < N; j++ ){
+			Mat[i][j]=(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+			if ( (abs(i - j) <= 3) && (i != j))
+				MatDD[i][j] = (((i*j)%3) ? -1 : 1)*(rand()/(1.0*RAND_MAX));
+			else if ( i == j )
+				 MatDD[i][j]=(((i*j)%3)?-1:1)*(10000.0*(rand()/(1.0*RAND_MAX)));
+			else MatDD[i][j] = 0.0;
+		}
+	for( i = 0; i < N; i++ ){
+		V1[i]=(i<N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+		V2[i]=(i>=N/2)?(((i*j)%3)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX))):0.0;
+		V3[i]=(((i*j)%5)?-1:1)*(100.0*(rand()/(1.0*RAND_MAX)));
+	}
+}
+
+
+void PrintVect( float vect[N], int from, int numel ) {
+	int printf
+
